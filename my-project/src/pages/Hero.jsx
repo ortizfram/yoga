f35ba9e124edcde0faadf53140ebb79482d98409
melon <ortizfranco48@gmail.com>
2023-03-09 20:15:54 -0300

    x

--- my-project/src/pages/Hero.jsx
+++ my-project/src/pages/Hero.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="bg-hero-image w-full h-screen bg-cover bg-center flex items-center px-6">
-      <div className="uppercase text-center text-white font-extrabold">
-        <h1 className="text-5xl m-auto sm:text-8xl">Kundalini</h1>
+      <div className="uppercase text-center text-white font-extrabold ">
+        <h1 className="text-5xl md:text-8xl sm:text-7xl">Kundalini</h1>
         <h3 className="text-2xl">para la vida.</h3>
       </div>
     </div>
