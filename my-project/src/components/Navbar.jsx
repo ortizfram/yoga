5fad045d6724c010a6086b5a8daf3c5261bbd942
melon <ortizfranco48@gmail.com>
2023-03-09 20:11:45 -0300

    hero responsive logo links

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -1,67 +1,27 @@
-import React, { useState } from "react";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import MenuIcon from "@mui/icons-material/Menu";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
-
-  const handleNav = () => {
-    setNav(!nav);
-  };
-
   return (
-    <div className="p-4 h-[10vh] shadow-lg">
-      {/* title & logo*/}
-
-      <h1 className="font-bold text-3xl text-gray-700">kM.</h1>
-
-      {/* page links */}
-      <ul className="hidden sm:flex justify-center">
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Classes</a>
-        </li>
-      </ul>
-
-      {/* hamburguer icon */}
-      <div onClick={handleNav} className="sm:hidden z-10">
-        <MenuIcon className="cursor-pointer" />
+    <div className="fixed w-full text-white flex justify-between p-4 items-center">
+      {/* logo */}
+      <div className="text-2xl font-bold text-center uppercase border-2 p-2">
+        <h1>
+          K <span className="block text-4xl">M</span>
+        </h1>
       </div>
 
-      {/* mobile menu */}
-      <div
-        onClick={handleNav}
-        className={
-          nav
-            ? "z-10 md:hidden ease-in duration-300 absolute text-gray-300 right-0 top-0 w-full h-screen bg-black/90 px-4 py-0.4 flex flex-col"
-            : "z-10 absolute top-0 h-screen left-[-100%] ease-in duration-500"
-        }
-      >
-        <ul className="h-full w-full text-center">
-          <li className="text-2xl py-4">
-            <a href="#home">Home</a>
-          </li>
-          <li className="text-2xl py-4">
-            <a href="#about">About</a>
-          </li>
-          <li className="text-2xl py-4">
-            <a href="#classes">Classes</a>
-          </li>
-          <li className="text-2xl py-6">
-            <a href="#register">Register</a>
-          </li>
-          <li className="text-2xl py-6">
-            <a href="#beamember">Be a member</a>
+      {/* nav */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+            <Link to="/">Acerca</Link>
+            <Link to="/">Talleres</Link>
+            <Link to="/">Contacto</Link>
           </li>
         </ul>
-      </div>
+      </nav>
     </div>
   );
 };
--- my-project/src/pages/Hero.jsx
+++ my-project/src/pages/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className="bg-hero-image w-full h-screen bg-cover bg-center flex items-center px-6">
       <div className="uppercase text-center text-white font-extrabold">
-        <h1 className="text-8xl">Kundalini</h1>
+        <h1 className="text-5xl m-auto sm:text-8xl">Kundalini</h1>
         <h3 className="text-2xl">para la vida.</h3>
       </div>
     </div>
