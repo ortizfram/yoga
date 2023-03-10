eba8d368f7031b7c2a145ad73ec3e9794c8b2cad
ortizfram <ortizfranco48@gmail.com>
2023-03-10 14:05:00 -0300

    contact responsive

--- my-project/src/pages/Contact.jsx
+++ my-project/src/pages/Contact.jsx
@@ -6,14 +6,14 @@ import EmailIcon from "@mui/icons-material/Email";
 const Contact = () => {
   return (
     //container
-    <div className="bg-contact-image bg-cover bg-center bg-brightness-50 p-6 w-full h-[50vh] mt-[300px] md:m-auto md:mt-[50px]">
+    <div className="bg-contact-image bg-cover bg-center bg-brightness-50 py-6 h-[50vh] mt-[310px] md:m-auto md:mt-[50px]">
       {/* section title */}
       <h1 className="p-4 text-2xl italic font-bold text-center text-white">
         Contacto.
       </h1>
 
       {/* container for icons */}
-      <div class="shadow-xl rounded-r-2xl rounded-t-2xl bg-black300/20 backdrop-blur-lg p-8 mx-[200px]">
+      <div class="sm:items-center sm:max-w-[432px] sm:flex sm:text-center sm:justify-between shadow-xl rounded-r-2xl rounded-t-2xl bg-black300/20 backdrop-blur-lg p-8 mx-[100px]">
         <InstagramIcon className="mx-5 text-white scale-150 hover:text-white/50 cursor-pointer" />
         <WhatsAppIcon className="mx-5 text-white scale-150 hover:text-white/50 cursor-pointer" />
         <EmailIcon className="mx-5 text-white scale-150 hover:text-white/50 cursor-pointer" />
