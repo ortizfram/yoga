e006a8a4009fc9a5af07f00f6c37f73f76dd5ac7
melon <ortizfranco48@gmail.com>
2023-03-10 18:55:09 -0300

    carousel responsive Z is left

--- my-project/src/components/Slider.jsx
+++ my-project/src/components/Slider.jsx
@@ -24,13 +24,14 @@ function Slider() {
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
-
+  // previous
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
 
+  // next
   const nextSlide = () => {
     const isLastSlide = currentIndex === slides.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
@@ -42,19 +43,24 @@ function Slider() {
   };
 
   return (
-    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
+    // container
+    <div className=" h-[20vh] w-[60vw] sm:max-w-[600px]  m-auto relative group shadow-2xl">
       <div
+        // link imgs
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
         className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
       ></div>
+
       {/* Left Arrow */}
-      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+      <div className="group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-1 p-2 text-white cursor-pointer">
         <ArrowBackIosNewIcon onClick={prevSlide} size={30} />
       </div>
       {/* Right Arrow */}
-      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+      <div className="group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-1 p-2 text-white cursor-pointer">
         <ArrowForwardIosIcon onClick={nextSlide} size={30} />
       </div>
+
+      {/* point mapping */}
       <div className="flex top-4 justify-center py-2">
         {slides.map((slide, slideIndex) => (
           <div
@@ -62,7 +68,7 @@ function Slider() {
             onClick={() => goToSlide(slideIndex)}
             className="text-2xl cursor-pointer"
           >
-            <FiberManualRecordIcon />
+            <FiberManualRecordIcon className="text-white/20" />
           </div>
         ))}
       </div>
